4a50a5d4093d00e134725b48a99e948fb55202a7
kvu-luong <luongkhanhvu1392@gmail.com>
2019-08-16 11:24:00 +0700

    add html home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    console.log("hello brother");
+ $('.js-latest_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+
+      $('.js-best_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
 })
